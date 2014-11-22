b12afc1781817121cf059626fdbf36440c17a64e
Ken Ohata <ohata@daraf.co.jp>
2014-11-22 19:22:17 +0900

    update sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,22 @@ class SessionsController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    email    = params_user[:email]
+    password = params_user[:password]
+
+    if login(email, password)
+      redirect_to root_url, notice: "successfully logged in."
+    else
+      @user = User.new(email: email)
+      render :new
+    end
+  end
+
+  private
+
+  def params_user
+    params.require(:user).permit(:email, :password)
+  end
 end
