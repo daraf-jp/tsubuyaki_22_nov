bdaca93d91e339b7f0918b2b16d42613324049bf
Ken Ohata <ohata@daraf.co.jp>
2014-11-22 19:08:13 +0900

    routes to sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   resource :registrations, only: [:new, :create]
+  resource :sessions, only: [:new, :create, :destroy]
 
   root to: 'registrations#new'
 
