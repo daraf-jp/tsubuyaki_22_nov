99191898021c15a95c0e12b00d2061dd88a0e7e7
Ken Ohata <ohata@daraf.co.jp>
2014-11-22 17:57:28 +0900

    add User validations
    fix User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[a-z][a-z0-9]+\z/ }, length: { in: 4..24 }
+  validates :screen_name, length: { maximum: 140 }
+  validates :bio, length: { maximum: 200  }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, confirmation: true, length: { in: 6..24 }, if: :password
+  validates :password_confirmation, presence: true, if: :password
 end
