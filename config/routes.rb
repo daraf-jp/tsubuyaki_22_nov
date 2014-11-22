237d5e9be9bd253a641b6408fec42f0414df12d2
Ken Ohata <ohata@daraf.co.jp>
2014-11-22 21:12:12 +0900

    remove users#new, create, edit, update, destroy

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resource :registrations, only: [:new, :create]
   resource :sessions, only: [:new, :create, :destroy]
-  resources :users
+  resources :users, only: [:index, :show]
 
   root to: 'registrations#new'
 
