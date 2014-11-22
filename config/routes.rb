a96245b1850777050b057d46f5e92efaf6e239eb
Ken Ohata <ohata@daraf.co.jp>
2014-11-22 17:55:08 +0900

    routes to registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'registrations/new'
+  resource :registrations, only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
