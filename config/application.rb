284ead711d14a75d9457d6d131fafc2f53037b69
Ken Ohata <ohata@daraf.co.jp>
2014-11-22 18:26:36 +0900

    config.i18n.default_locale = :jp
    fix config.i18n.default_locale = :ja

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Tsubuyaki22Nov
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :ja
 
     config.sass.preferred_syntax = :sass
   end
