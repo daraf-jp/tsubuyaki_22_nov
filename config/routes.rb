c71c645311742874487cb7c76299f5895cd137de
Ken Ohata <ohata@daraf.co.jp>
2014-11-22 18:01:44 +0900

    define root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resource :registrations, only: [:new, :create]
 
+  root to: 'registrations#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
