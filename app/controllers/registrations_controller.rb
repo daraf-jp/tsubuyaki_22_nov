34505a9ef62dd18659f18712e0ea30879b742277
Ken Ohata <ohata@daraf.co.jp>
2014-11-22 18:02:15 +0900

    update registrations#create
    [fix] root_path to root_url

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,4 +2,20 @@ class RegistrationsController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(params_user)
+
+    if @user.save
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_user
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
