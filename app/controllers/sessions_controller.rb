1e9183940a9cefb1ad0c36e200ba67a50286ddf6
Ken Ohata <ohata@daraf.co.jp>
2014-11-23 15:16:35 +0900

    change sessions#create notice message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     password = params_user[:password]
 
     if login(email, password)
-      redirect_to root_url, notice: "successfully logged in."
+      redirect_to root_url, notice: "サインインしました"
     else
       @user = User.new(email: email)
       render :new
