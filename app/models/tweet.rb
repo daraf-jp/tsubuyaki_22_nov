518a5a5de8ca077b6c7736cd24eee0f8bb7174e7
Ken Ohata <ohata@daraf.co.jp>
2014-11-23 17:05:58 +0900

    define Tweet default_scope

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,4 +3,6 @@ class Tweet < ActiveRecord::Base
 
   validates :user, presence: true
   validates :content, presence: true, length: { in: 1..140 }
+
+  default_scope -> { order(created_at: :desc) }
 end
