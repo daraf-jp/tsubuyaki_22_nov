d27d606a80162cd0be5f14760188e623197fe76d
Ken Ohata <ohata@daraf.co.jp>
2014-11-23 17:28:30 +0900

    define favorites#create
    fix message create

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,14 @@
 class FavoritesController < ApplicationController
+  before_filter :require_login
+
+  def create
+    @tweet    = Tweet.find(params[:tweet_id])
+    @favorite = current_user.favorites.build(tweet: @tweet)
+
+    if @favorite.save
+      redirect_to tweets_url, notice: "お気に入りに登録しました"
+    else
+      redirect_to tweets_url, alert: "このツイートはお気に入りに登録できません"
+   end
+  end
 end
