3b8aa76682a4c7e8cef818e81b51b938bd65611c
Ken Ohata <ohata@daraf.co.jp>
2014-11-23 18:15:54 +0900

    add Follow validations

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,8 @@
 class Follow < ActiveRecord::Base
   belongs_to :follower, class_name: User
   belongs_to :inverse_follower, class_name: User
+
+  validates :follower, presence: true
+  validates :inverse_follower, presence: true
+  validates :inverse_follower_id, uniqueness: { scope: :follower_id }
 end
