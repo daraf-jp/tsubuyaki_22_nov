44d418f45b497bbd198c4f7094d146c51749842b
Ken Ohata <ohata@daraf.co.jp>
2014-11-23 17:01:52 +0900

    update redirect path

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -31,7 +31,7 @@ class TweetsController < ApplicationController
 
     respond_to do |format|
       if @tweet.save
-        format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
+        format.html { redirect_to tweets_url, notice: 'Tweet was successfully created.' }
         format.json { render :show, status: :created, location: @tweet }
       else
         format.html { render :new }
