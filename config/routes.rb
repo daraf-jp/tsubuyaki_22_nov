40cd08f567bf714247e754c56f0be4ad1b941d9c
Ken Ohata <ohata@daraf.co.jp>
2014-11-23 11:49:26 +0900

    routes to settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'settings/edit'
-
   resource :registrations, only: [:new, :create]
   resource :sessions, only: [:new, :create, :destroy]
+  resource :settings, only: [:edit, :update]
   resources :users, only: [:index, :show]
 
   root to: 'registrations#new'
