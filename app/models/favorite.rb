0015b08b0814107e06781b444dde4b670c6c0076
Ken Ohata <ohata@daraf.co.jp>
2014-11-23 17:25:29 +0900

    add Favorite validations
    add Favorite validations

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,8 @@
 class Favorite < ActiveRecord::Base
   belongs_to :user
   belongs_to :tweet
+
+  validates :user, presence: true
+  validates :user_id, uniqueness: { scope: :tweet_id }
+  validates :tweet, presence: true
 end
