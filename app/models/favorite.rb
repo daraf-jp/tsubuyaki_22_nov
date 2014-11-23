304e37490537e468758ce0513531a062f83210f5
Ken Ohata <ohata@daraf.co.jp>
2014-11-23 17:24:34 +0900

    assoc Favorite, User and Tweet

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,2 +1,4 @@
 class Favorite < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :tweet
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,6 @@
 class Tweet < ActiveRecord::Base
   belongs_to :user
+  has_many :favorites, dependent: :destroy
 
   validates :user, presence: true
   validates :content, presence: true, length: { in: 1..140 }
