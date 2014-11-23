3bb8e6446b04b25d118e32ad4b244a27f8da828e
Ken Ohata <ohata@daraf.co.jp>
2014-11-23 11:49:00 +0900

    rails g controller settings edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'settings/edit'
+
   resource :registrations, only: [:new, :create]
   resource :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:index, :show]
