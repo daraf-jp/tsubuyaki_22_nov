ebd7c0b0924ccc2c1f0165d5270162600b82f7be
Ken Ohata <ohata@daraf.co.jp>
2014-11-23 16:33:20 +0900

    fix typo at Tweet validation

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,5 +2,5 @@ class Tweet < ActiveRecord::Base
   belongs_to :user
 
   validates :user, presence: true
-  validates :content, precence: true, length: { in: 1..140 }
+  validates :content, presence: true, length: { in: 1..140 }
 end
