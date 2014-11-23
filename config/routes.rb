252ca753655ceba8fcf5d42e648f4f8781388c2f
Ken Ohata <ohata@daraf.co.jp>
2014-11-23 17:35:29 +0900

    routes to users#favorites

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   resource :registrations, only: [:new, :create]
   resource :sessions, only: [:new, :create, :destroy]
   resource :settings, only: [:edit, :update]
-  resources :users, only: [:index, :show]
+
+  resources :users, only: [:index, :show] do
+    get :favorites, on: :member
+  end
+
   resources :tweets do
     resource :favorites, only: [:create, :destroy]
   end
