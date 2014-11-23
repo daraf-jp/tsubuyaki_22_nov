abbcba983661daca9205f446962676b707ce180b
Ken Ohata <ohata@daraf.co.jp>
2014-11-23 12:08:21 +0900

    define settings#update

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -2,4 +2,16 @@ class SettingsController < ApplicationController
   def edit
     @user = current_user
   end
+
+  def update
+    @user = current_user
+    @user.update_attributes(params_user)
+    redirect_to user_url(@user)
+  end
+
+  private
+
+  def params_user
+    params.require(:user).permit(:name, :screen_name, :bio)
+  end
 end
