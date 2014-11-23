fe7c1764ba8c9cc835ce251e405588df2f0e0bc6
Ken Ohata <ohata@daraf.co.jp>
2014-11-23 18:44:07 +0900

    define follows#create

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,2 +1,13 @@
 class FollowsController < ApplicationController
+  before_filter :require_login
+
+  def create
+    @user = User.find(params[:user_id])
+
+    if @user.inverse_follows.create(follower: current_user)
+      redirect_to tweets_url, notice: "フォローしました"
+    else
+      redirect_to tweets_url, alert: "フォローできません"
+    end
+  end
 end
