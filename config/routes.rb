53111b192ee08599e834c79a32b827f35f62a97d
Ken Ohata <ohata@daraf.co.jp>
2014-11-23 16:29:08 +0900

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :tweets
-
   resource :registrations, only: [:new, :create]
   resource :sessions, only: [:new, :create, :destroy]
   resource :settings, only: [:edit, :update]
   resources :users, only: [:index, :show]
+  resources :tweets
 
   root to: 'registrations#new'
 
