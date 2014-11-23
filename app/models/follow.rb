9dab97884634fd94e1778b738a3322a81dc9a75c
Ken Ohata <ohata@daraf.co.jp>
2014-11-23 18:14:15 +0900

    assoc Follow and User
    fix assoc

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,2 +1,4 @@
 class Follow < ActiveRecord::Base
+  belongs_to :follower, class_name: User
+  belongs_to :inverse_follower, class_name: User
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ActiveRecord::Base
   has_many :tweets, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  has_many :follows, foreign_key: :follower_id
+  has_many :inverse_followers, through: :follows
+  has_many :inverse_follows, foreign_key: :inverse_follower_id, class_name: Follow
+  has_many :followers, through: :inverse_follows
+
   validates :name, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[a-z][a-z0-9]+\z/ }, length: { in: 4..24 }
   validates :screen_name, length: { maximum: 140 }
   validates :bio, length: { maximum: 200  }
