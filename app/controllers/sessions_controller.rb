0dcdf1ea9d2a2e62adbbe521e47911610bccdf49
Ken Ohata <ohata@daraf.co.jp>
2014-11-24 13:51:00 +0900

    fix sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     password = params_user[:password]
 
     if login(email, password)
-      redirect_to root_url, notice: "サインインしました"
+      redirect_to tweets_url, notice: "サインインしました"
     else
       @user = User.new(email: email)
       render :new
